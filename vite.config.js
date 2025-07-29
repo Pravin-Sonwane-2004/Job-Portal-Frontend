7b7d8369faa2b3f7e4490f6716ae796d3546df27
Pravin-Sonwane-2004 <pravinson222@gmail.com>
2025-07-29 14:53:17 +0530

    But Still There Is Error In Docker Changes Are Not Reflectingg

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+      host: true,   // important!
     port: 3000,
+        watch: {
+      usePolling: true,    // 👈 required for Docker volume
+    }
   },
   css: {
     postcss: './postcss.config.js'
